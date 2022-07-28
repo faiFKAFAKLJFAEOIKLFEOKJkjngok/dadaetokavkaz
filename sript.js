66aa485e438da23a68ab7cc33bd300b2c8414ef3
faiFKAFAKLJFAEOIKLFEOKJkjngok <110091628+faiFKAFAKLJFAEOIKLFEOKJkjngok@users.noreply.github.com>
2022-07-28 12:08:33 +0300

    Add files via upload

--- sript.js
+++ sript.js
@@ -6,7 +6,17 @@ b1.onclick = function() {
 }
 
 var b2 = document.getElementById("b2")
+b2.onclick = function() {
+    h11.style.color = "green"
+} 
 
 var num = 38 % 5;
 console.log(num)
 console.log("Hello World!")
+
+function run(){
+    while (i < 3) {//выводит 0, затем 1, затем 2} 
+    console.log(i);
+    i++;
+}
+}
